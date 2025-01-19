7049aedc11c6c01c08fd66fc67045c8358ddcb4e
lulook <lulook23@gmail.com>
2025-01-19 20:31:33 +0100

    def-> for defining threading macros

--- macs.lisp
+++ macs.lisp
@@ -1,23 +1,23 @@
-(defmacro -> (&body exprs)
-  (reduce
-    (lambda (acc e)
-      (if (listp e)
-        (apply #'list (first e) acc (rest e))
-        (list e acc)
+(defmacro def-> (name params list-case non-list-case)
+  `(defmacro ,name (&body exprs)
+    (reduce
+      (lambda ,params
+        (if (listp ,(second params))
+          ,list-case
+          ,non-list-case
+        )
       )
+      exprs
     )
-    exprs
   )
 )
 
-(defmacro ->> (&body exprs)
-  (reduce
-    (lambda (acc e)
-      (if (listp e)
-        (append e (list acc))
-        (list e acc)
-      )
-    )
-    exprs
-  )
+(def-> 1-> (acc e)
+  (apply #'list (first e) acc (rest e))
+  (list e acc)
+)
+
+(def-> last-> (acc e)
+  (append e (list acc))
+  (list e acc)
 )
